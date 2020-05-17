e4e650dfa706a9724c1dd69c7810942db2c5e809
Milton Câmara <miltomcamara@gmail.com>
2020-05-17 16:20:49 -0300

    Commit Inicial

---
+++ GitHubActions_Example.Tests/TestExample.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace GitHubActions_Example.Tests
+{
+    public class TestExample
+    {
+        [Fact]
+        public void Test_True()
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
